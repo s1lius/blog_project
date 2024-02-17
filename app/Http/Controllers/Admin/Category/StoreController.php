00ec2cc006919396993afa3604085853f8130018
Em <emir.murtazaev.01@gmail.com>
2024-02-17 18:12:44 +0200

    Implemented an update for posts, an editing form, displaying images, deleting posts, using soft delete, fixed delete error

--- app/Http/Controllers/Admin/Category/StoreController.php
+++ app/Http/Controllers/Admin/Category/StoreController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Admin\Category;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Admin\Post\StoreRequest;
+use App\Http\Requests\Admin\Category\StoreRequest;
 use App\Models\Category;
 
 class StoreController extends Controller
--- resources/views/admin/post/edit.blade.php
+++ resources/views/admin/post/edit.blade.php
@@ -25,23 +25,91 @@
             <div class="container-fluid">
                 <!-- Small boxes (Stat box) -->
                 <div class="row">
-                    <div class="col-4">
+                    <div class="col-8">
                         <div class="card card-primary">
                             <!-- /.card-header -->
                             <!-- form start -->
-                            <form action="{{route('admin.post.update', $post->id)}}" method="post">
+                            <form action="{{route('admin.post.update', $post->id)}}" method="post" enctype="multipart/form-data">
                                 @csrf
                                 @method('patch')
                                 <div class="card-body">
                                     <div class="form-group">
-                                        <label>Название</label>
-                                        <input type="text" class="form-control" name="title"
-                                               placeholder="Введите новое название категории"
-                                               value="{{$post->title}}">
-                                        @error('title')
+                                        <div class="form-group w-25">
+                                            <label>Название</label>
+                                            <input type="text" class="form-control" name="title"
+                                                   placeholder="Введите название поста" value="{{$post->title}}">
+                                            @error('title')
                                             <div class="text-danger">Заполните поле</div>
-                                        @enderror
-                                        <input type="submit" class="btn btn-primary mt-3" value="Изменить">
+                                            @enderror
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Контент</label>
+                                            <textarea id="summernote" name="content">{{$post->content}}</textarea>
+                                            @error('content')
+                                            <div class="text-danger">Заполните поле</div>
+                                            @enderror
+                                        </div>
+                                        <div class="form-group w-50">
+                                            <label for="exampleInputFile">Добавить превью</label>
+                                            <div class="w-75 mb-3">
+                                                <img src="{{url('storage/' . $post->preview_image)}}" alt="preview_image" class="w-50">
+                                            </div>
+                                            <div class="input-group">
+                                                <div class="custom-file">
+                                                    <input type="file" class="custom-file-input" name="preview_image">
+                                                    <label class="custom-file-label">Выберите изображение</label>
+                                                </div>
+                                                <div class="input-group-append">
+                                                    <span class="input-group-text">Загрузка</span>
+                                                </div>
+                                            </div>
+                                            @error('preview_image')
+                                            <div class="text-danger">Заполните поле</div>
+                                            @enderror
+                                        </div>
+                                        <div class="form-group w-50">
+                                            <label for="exampleInputFile">Добавить главное изображение</label>
+                                            <div class="w-75 mb-3">
+                                                <img src="{{url('storage/' . $post->main_image)}}" alt="main_image" class="w-50">
+                                            </div>
+                                            <div class="input-group">
+                                                <div class="custom-file">
+                                                    <input type="file" class="custom-file-input" name="main_image">
+                                                    <label class="custom-file-label">Выберите изображение</label>
+                                                </div>
+                                                <div class="input-group-append">
+                                                    <span class="input-group-text">Загрузка</span>
+                                                </div>
+                                            </div>
+                                            @error('main_image')
+                                            <div class="text-danger">Заполните поле</div>
+                                            @enderror
+                                        </div>
+                                        <div class="form-group w-50">
+                                            <label>Выбрать катеорию</label>
+                                            <select name="category_id" class="form-control">
+                                                @foreach($categories as $category)
+                                                    <option value="{{$category->id}}"
+                                                        {{$category->id == $post->category_id ? 'selected' : ''}}
+                                                    >{{$category->title}}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                        <div class="form-group w-50">
+                                            <label>Выбрать тэги</label>
+                                            <select class="select2" name="tag_ids[]" multiple="multiple"
+                                                    data-placeholder="Выберите тэги"
+                                                    style="width: 100%;">
+                                                @foreach($tags as $tag)
+                                                    <option
+                                                        {{is_array($post->tags->pluck('id')->toArray()) && in_array($tag->id, $post->tags->pluck('id')->toArray()) ? 'selected' : ''}}
+                                                        value="{{$tag->id}}">{{$tag->title}}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="submit" class="btn btn-primary mt-3" value="Обновить">
+                                        </div>
                                     </div>
                                 </div>
                                 <!-- /.card-body -->
